953642dcb65ff36a0f483ce9eb0706008cb26b9e
Furuikeya <furuburi@gmail.com>
2020-09-15 20:22:28 +0300

    real friends from js file version

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Ekaterina Pavlova',
+		username: 'Soyabean',
+		email: 'eastina@mushroom.vu'
+	},
+	{
+		id: 2,
+		name: 'Roman Kostyuk',
+		username: 'Ramasta',
+		email: 'korneplod@wot.su'
+	},
+	{
+		id: 3,
+		name: 'Valeriy Kudryavtsev',
+		username: 'Voltron',
+		email: 'guinness@pub.org'
+	},
+	{
+		id: 4,
+		name: 'Pedro Rodrigues',
+		username: 'Pedro',
+		email: 'sittingallday@papillon.co.in'
+	},
+	{
+		id: 5,
+		name: 'Stanislav Smolyakov',
+		username: 'Staaarz',
+		email: 'travelmultiverse@home.com'
+	},
+	{
+		id: 6,
+		name: 'Aleksei Mahrov',
+		username: 'Professor',
+		email: 'kolotit@papillon.co.in'
+	},
+	{
+		id: 7,
+		name: 'Grigory Sevastyanov',
+		username: 'Tovsl',
+		email: 'darkelfarchitect@aden.li'
+	},
+	{
+		id: 8,
+		name: 'Yulia Sidorova',
+		username: 'linell',
+		email: 'fotkaet@cvetochki.gb'
+	},
+	{
+		id: 9,
+		name: 'Vadim Shkadov',
+		username: 'V',
+		email: 'dumaetshto@smirnoff.ha'
+	},
+	{
+		id: 10,
+		name: 'Katya Lyopina-Kladovaya',
+		username: 'Lyopa',
+		email: 'varitimnyot@home.kh'
+	},
+	{
+		id: 11,
+		name: 'Bar Keshet',
+		username: 'PsychoPhisic',
+		email: 'flyingonahummuscloud@ashram.co.iz'
+	},
+	{
+		id: 12,
+		name: 'Viktor Gavriliuk',
+		username: 'ZhenaZvonit',
+		email: 'straher-braher@khmarachyos.by'
+	},
+	{
+		id: 13,
+		name: 'Rvins Varde',
+		username: 'Froggy',
+		email: 'bird@forest.lv'
+	}
+]
